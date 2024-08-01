532d111e3a3a2a21ce688590670849b80d40dbc8
AshwinDEM <ashwindharmavaram@gmail.com>
2024-08-01 21:45:13 +0530

    Saved user information in a csv file

--- main.py
+++ main.py
@@ -1,26 +1,38 @@
-import os
 import streamlit as st
 import pandas as pd
+import os
+
+# Path to the users CSV file
+USER_FILE_PATH = "./users/users.csv"
 
 # Initialize session state variables
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
     st.session_state.username = ""
-    st.session_state.users = {}
     st.session_state.registering = False
+
+# Function to read users from CSV
+def read_users():
+    if os.path.exists(USER_FILE_PATH):
+        return pd.read_csv(USER_FILE_PATH)
+    else:
+        return pd.DataFrame(columns=["username", "password"])
+
+# Function to add a user to the CSV
+def add_user(username, password):
+    users_df = read_users()
+    new_user = pd.DataFrame({"username": [username], "password": [password]})
+    users_df = pd.concat([users_df, new_user], ignore_index=True)
+    users_df.to_csv(USER_FILE_PATH, index=False)
 
 # User authentication function
 def login(username, password):
-    # For simplicity, we use hardcoded credentials
-    if username in st.session_state.users:
-        return st.session_state.users[username] == password
-    else:
-        return False
-
-def add_user(username, password):
-    #st.session_state.users[username] = str(hash(password))
-    pass
-
+    users_df = read_users()
+    if not users_df.empty:
+        user_row = users_df[users_df['username'] == username]
+        if not user_row.empty and user_row.iloc[0]['password'] == password:
+            return True
+    return False
 
 # Streamlit app
 def main():
@@ -48,12 +60,12 @@ def main():
         new_password = st.text_input("New Password", type="password")
         confirm_password = st.text_input("Confirm Password", type="password")
         if st.button("Register"):
-            if new_username in st.session_state.users:
+            if new_username in read_users()['username'].values:
                 st.error("Username already exists")
             elif new_password != confirm_password:
                 st.error("Passwords do not match")
             else:
-                st.session_state.users[new_username] = new_password
+                add_user(new_username, new_password)
                 st.success("Registered successfully! Please log in.")
                 st.session_state.registering = False
         if st.button("Back to Login"):
@@ -65,8 +77,8 @@ def main():
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = []
 
-        # Select which user is currently chatting
-        users_list = list(st.session_state.users.keys())
+        # Read users from CSV and prepare the users list
+        users_list = read_users()['username'].tolist()
         users_list.remove(st.session_state.username)
         if not users_list:
             st.warning("No other users to chat with")
